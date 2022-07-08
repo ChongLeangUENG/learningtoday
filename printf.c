cbb5aa125d4eee374662fde3a789a9351eef0dbf
ChongLeangUENG <105638150+ChongLeangUENG@users.noreply.github.com>
2022-07-08 16:18:20 +0200

    Update printf.c

--- printf.c
+++ printf.c
@@ -1,62 +1,77 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 
+/**
+* print_format - Print all format
+* @format: Format to print
+* return: If success.
+*/
 int (*print_format(const char *format))(va_list)
 {
-	unsigned int j;
-
-	print_t p[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{NULL, NULL}
+	int j = 0;
+	print_t type[] = {
+			{"c", print_c},
+			{"s", print_s},
+			{NULL, NULL}
 	};
 
-	for (j = 0; p[j].s != NULL; j++)
+	for (; type[j].s != NULL; j++)
 	{
-		if (*(p[j].s) == *format)
-		{
+		if (*(type[j].s) == *format)
 			break;
-		}
 	}
-	return (p[j].i);
+	return (type[j].i);
 }
+
+/**
+* _printf - function that produces output of format
+* @format: a pointer to produce
+* @...: others
+* Return: the number of characters print
+*/
 
 int _printf(const char *format, ...)
 {
-	unsigned int x = 0; count = 0;
 	va_list list;
 	int (*f)(va_list);
+	unsigned int y = 0, x = 0;
 
 	if (format == NULL)
 		return (-1);
+
 	va_start(list, format);
-	while (format[x])
+
+	while (format && format[y])
 	{
-		for (; format[x] != '%' && format[x]; x++)
+		if (format[y] != '%' || format[y] != '\0')
 		{
-			_putchar(format[x]);
-			count++;
-		}
-		if (!format[x])
-			return (count);
-		f = print_format(&format[x + 1]);
-		if (f != NULL)
-		{
-			count += f(list);
-			x += 2;
+			_putchar(format[y]);
+			y++;
+			x++;
 			continue;
 		}
-		if (!format[x + 1])
-			return (-1);
-		_putchar(format[x]);
-		count++;
-
-		if (format[x + 1] == '%')
-			x += 2;
 		else
-			x++;
+		{
+			if (format[y + 1] == '%')
+			{
+				_putchar('%');
+				x++;
+				y += 2;
+				continue;
+			}
+			else
+			{
+				f = print_format(&format[y + 1]);
+				if (f == NULL)
+					return (-1);
+				y += 2;
+				x += f(list);
+				continue;
+			}
+		}
+		y++;
 	}
 	va_end(list);
-	return (count);
+	return (x);
 }
